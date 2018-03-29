8a6b7e189f3bcf3aafecf93aca3765857a6a6538
Hugh Gallagher <hugh@salesmaster.co.uk>
2018-03-29 15:22:21 +0100

    Split bash_profile by host

--- setup_symlinks.sh
+++ setup_symlinks.sh
@@ -1,2 +1,6 @@
-ln -si ~/dotfiles/bash_profile ~/.bash_profile
 ln -si ~/dotfiles/vimrc ~/.vimrc
+ln -si ~/dotfiles/bash_profile ~/.bash_profile
+
+if [ "$HOSTNAME" = "morbo.local" ]; then
+  ln -si ~/dotfiles/bash_profile_morbo ~/.bash_local
+fi
